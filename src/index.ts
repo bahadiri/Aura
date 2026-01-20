35cae605ad5c1d6f41603ae51a60002063e2aa6d
Bahadir Ismail Aydin <bahadir.ismail.aydin@gmail.com>
2026-01-19 20:42:18 -0500

    chore: setup npm publishing pipeline

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Core SDK exports
+export * from './sdk';
+
+// Atmosphere AIRs - import and re-export with clear names
+export { Component as PlotAIR } from './atmosphere/plot';
+export { Component as CharacterAIR } from './atmosphere/characters';
+export { Component as ImageAIR } from './atmosphere/image';
+export { Component as NoteTakerAIR } from './atmosphere/note-taker';
+export { Component as TasksAIR } from './atmosphere/tasks';
+export { Component as YouTubePlayerAIR } from './atmosphere/youtube-player';
+
+// Hooks
+export { useVoiceInput } from './hooks/useVoiceInput';
+
+// Storage
+export type { AuraStorageConfig } from './storage/config';
